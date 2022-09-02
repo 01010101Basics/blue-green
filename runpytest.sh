1a2f5318cdaab75c0dc2c8af33ca97e060e00988
Ken Goddard <closerwalk@gmail.com>
2022-09-01 22:14:13 -0700

    Create runpytest.sh

---
+++ runpytest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+####Creating the Display Port#######
+export DISPLAY=:99
+echo "The Display is: $DISPLAY"
+# Creating the Xvfb desktop ###########
+Xvfb :99 -ac -screen 0 1280x1024x24 &
+# Run the browser test ################
+python3 testsite.py
+# Cleanup after #######################
+sudo killall Xvfb
