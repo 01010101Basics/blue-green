2ee2e30b021855d3c792dc9263d8ab75da55f4e8
Ken Goddard <closerwalk@gmail.com>
2022-09-03 18:13:42 +0000

    updated version

--- runpytest.sh
+++ runpytest.sh
@@ -5,6 +5,7 @@ echo "The Display is: $DISPLAY"
 # Creating the Xvfb desktop ###########
 Xvfb :99 -ac -screen 0 1280x1024x24 &
 # Run the browser test ################
-python3 testsite.py
+python3 -m pytest testsite.py --junit-xml=pytest_unit.xml
+#python3 testsite.py
 
 
--- testsite.py
+++ testsite.py
@@ -13,12 +13,12 @@ chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
-driver = webdriver.Chrome(chrome_options=chrome_options)
+driver = webdriver.Chrome(options=chrome_options)
 
 import os
 import json
 import time
-
+import pytest
 driver.get("http://10.0.0.16:82/")
 elem = driver.find_element(By.XPATH, "HTML/BODY")
 
